2070516745172c520cb49ff2cb951e7977f02192
vadimgaidai <vgaidai@codyua.com>
2022-04-25 21:13:43 +0300

    feat: Implement DB connect

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { UsersModule } from './users/users.module';
-import { AuthenticationModule } from './authentication/authentication.module';
+
+import { UsersModule } from './users/users.module'
+import { AuthenticationModule } from './authentication/authentication.module'
 
 @Module({
-  imports: [UsersModule, AuthenticationModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign({
+          type: 'mysql',
+          host: process.env.DB_HOST,
+          port: process.env.DB_PORT,
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_NAME,
+          synchronize: true,
+          autoLoadEntities: true,
+        }),
+    }),
+    UsersModule,
+    AuthenticationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,1 +1,27 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm'
+
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  name: string
+
+  @Column()
+  surname: string
+
+  @Column({
+    unique: true,
+  })
+  email: string
+
+  @Column({ nullable: true })
+  public password?: string
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt: Date
+}
