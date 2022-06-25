0f2b59e9e612b1ec6400b8d6eafcfada2d8478cc
vadimgaidai <vgaidai@codyua.com>
2022-06-25 23:27:32 +0300

    fix: Resolve base patchs

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-import theme from '../theme'
+import theme from 'styles/theme'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <ChakraProvider theme={theme}>
--- frontend/styles/theme.ts
+++ frontend/styles/theme.ts
@@ -2,13 +2,7 @@
 import { extendTheme } from '@chakra-ui/react'
 
 // 2. Extend the theme to include custom colors, fonts, etc
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
-  },
-}
+const colors = {}
 
 const theme = extendTheme({ colors })
 
