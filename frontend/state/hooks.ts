569edcfdd9cacf39374491bc7a47dd28c7d22a6b
vadimgaidai <vgaidai@codyua.com>
2022-07-10 14:08:33 +0300

    feat: Add next-redux-wrapper

---
+++ frontend/state/hooks.ts
@@ -0,0 +1,6 @@
+import { useDispatch } from 'react-redux'
+
+/**
+ * @see https://redux-toolkit.js.org/usage/usage-with-typescript#getting-the-dispatch-type
+ */
+export const useAppDispatch = () => useDispatch()
